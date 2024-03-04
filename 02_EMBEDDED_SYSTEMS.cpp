92d2144ef2dace31bc667297dc3d3504468d2deb
Trishita Yadav <119665769+triishita@users.noreply.github.com>
2024-03-04 17:42:26 +0530

    Update 02_EMBEDDED_SYSTEMS.cpp

--- 02_EMBEDDED_SYSTEMS.cpp
+++ 02_EMBEDDED_SYSTEMS.cpp
@@ -1,11 +1,10 @@
-// C++ code
-//
+//C++ code to print Hello world
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600); //9600 is baud rate
+  //Baud rate=Maximum oscillation rate of an electronic signal
 }
-
 void loop()
 {
-  Serial.print("Hello World");
+  Serial.print("Hello World"); //this is the final output
 }
